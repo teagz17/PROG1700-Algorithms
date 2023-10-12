e428629c36c49c27c31b650824a7d6411068d34a
teagz17 <146100819+teagz17@users.noreply.github.com>
2023-10-12 15:16:03 -0300

    initial commit

---
+++ class_activities_and_exercises/PROG1700_List_and_String_exercises.py
@@ -0,0 +1,20 @@
+"""
+PROG1700
+lists and string exercise
+
+you are given a list of names in a list data structure,
+names = ["Mike, "Harper", "Tike", "Carter", "Porter", "Parker"]
+
+work backwords through the list and print the names backwards like so:
+rekraP
+retroP
+retraC
+ekiT
+repraH
+ekiM
+"""
+
+
+names = ["Mike", "Harper", "Tike", "Carter", "Porter", "Parker"]
+for name in names:
+    print(name)
